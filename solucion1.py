352c91af0c251f58683643025e6ade53c94c3397
miguelangel1200 <75793818+miguelangel1200@users.noreply.github.com>
2022-08-26 09:45:07 -0500

    Add files via upload

---
+++ solucion1.py
@@ -0,0 +1,14 @@
+
+from multiprocessing.dummy import Array
+
+
+lista = []
+num = input("Ingrese una hora: ")
+
+test = "hola"
+contador = 0
+
+while contador <= 10:
+    print(contador)
+    contador+=1
+
